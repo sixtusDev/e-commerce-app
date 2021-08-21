ad73f6fcb9c33c453cfbd06dd06eb51627334226
Sixtus Innocent <sixtusinno@gmail.com>
2021-08-21 10:57:28 +0100

    map dispatch to props for current user

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@
 // Modules
 import React from "react";
 import { Switch, Route } from "react-router-dom";
+import { connect } from "react-redux";
 
 // External Scripts
 import HomePage from "./pages/homepage/homepage.component";
 import ShopPage from "./pages/shop/shop.component";
 import SignInAndSignUpPage from "./pages/sign-in-and-sign-up/sign-in-and-sign-up.component";
 import Header from "./components/header/header.component";
+import { setCurrentUser } from "./redux/user/user-actions";
 import { auth, createUserProfileDocument } from "./firebase/firebase.utils";
 
 // Styles
@@ -16,21 +18,23 @@ class App extends React.Component {
   unsubscribeFromAuth = null;
 
   componentDidMount() {
+    const { setCurrentUser } = this.props;
     this.unsubscribeFromAuth = auth.onAuthStateChanged(async (userAuth) => {
       if (userAuth) {
         const userRef = await createUserProfileDocument(userAuth);
 
         userRef.onSnapshot((snapShot) => {
-          this.setState({
+          setCurrentUser({
             currentUser: {
               id: snapShot.id,
               ...snapShot.data(),
             },
           });
         });
+        console.log(userRef);
       }
 
-      this.setState({ currentUser: userAuth });
+      setCurrentUser({ currentUser: userAuth });
     });
   }
 
@@ -52,4 +56,8 @@ class App extends React.Component {
   }
 }
 
-export default App;
+const mapDispatchToProps = (dispatch) => ({
+  setCurrentUser: (user) => dispatch(setCurrentUser(user)),
+});
+
+export default connect(null, mapDispatchToProps)(App);
