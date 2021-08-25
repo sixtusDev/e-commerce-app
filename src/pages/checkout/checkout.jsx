3c4930a058c52cdddbd3127c42978bc59cd42db0
Sixtus Innocent <sixtusinno@gmail.com>
2021-08-25 20:52:54 +0100

    Added selector for total amount of cart item

--- src/pages/checkout/checkout.jsx
+++ src/pages/checkout/checkout.jsx
@@ -1,9 +1,35 @@
+import { connect } from "react-redux";
+import { createStructuredSelector } from "reselect";
+
+import { selectCartItems } from "../../redux/cart/cart-selectors";
+
 import "./checkout.scss";
 
 const CheckoutPage = () => (
-  <div>
-    <span>Checkout page</span>
+  <div className="checkout-page">
+    <div className="checkout-header">
+      <div className="header-block">
+        <span>Product</span>
+      </div>
+      <div className="header-block">
+        <span>Description</span>
+      </div>
+      <div className="header-block">
+        <span>Quantity</span>
+      </div>
+      <div className="header-block">
+        <span>Price</span>
+      </div>
+      <div className="header-block">
+        <span>Remove</span>
+      </div>
+    </div>
+    <div className="total">TOTAL: $78</div>
   </div>
 );
 
-export default CheckoutPage;
+const mapStateToProps = createStructuredSelector({
+  selectCartItems,
+});
+
+export default connect(mapStateToProps)(CheckoutPage);
