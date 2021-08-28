d12d80be76c289ef0fb0249da65c15e2ea769636
Sixtus Innocent <sixtusinno@gmail.com>
2021-08-28 13:18:53 +0100

    Added checkout item

--- src/pages/checkout/checkout.jsx
+++ src/pages/checkout/checkout.jsx
@@ -2,10 +2,11 @@ import { connect } from "react-redux";
 import { createStructuredSelector } from "reselect";
 
 import { selectCartItems } from "../../redux/cart/cart-selectors";
+import ChekoutItem from "../../components/checkout-item/checkout-item";
 
 import "./checkout.scss";
 
-const CheckoutPage = () => (
+const CheckoutPage = ({ cartItems }) => (
   <div className="checkout-page">
     <div className="checkout-header">
       <div className="header-block">
@@ -24,12 +25,15 @@ const CheckoutPage = () => (
         <span>Remove</span>
       </div>
     </div>
+    {cartItems.map((cartItem) => (
+      <ChekoutItem key={cartItem.id} cartItem={cartItem} />
+    ))}
     <div className="total">TOTAL: $78</div>
   </div>
 );
 
 const mapStateToProps = createStructuredSelector({
-  selectCartItems,
+  cartItems: selectCartItems,
 });
 
 export default connect(mapStateToProps)(CheckoutPage);
