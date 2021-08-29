04a6372f7dab7e491f813f26ff2c9d4cedb10974
Sixtus Innocent <sixtusinno@gmail.com>
2021-08-29 12:51:59 +0100

    Added directory reducer

--- src/redux/root-reducer.js
+++ src/redux/root-reducer.js
@@ -7,6 +7,7 @@ import storage from "redux-persist/lib/storage";
 // External Scripts
 import userReducer from "./user/user-reducer";
 import cartReducer from "./cart/cart-reducer";
+import directoryReducer from "./directory/directory-reducer";
 
 const persistConfig = {
   key: "root",
@@ -17,6 +18,7 @@ const persistConfig = {
 const rootReducer = combineReducers({
   user: userReducer,
   cart: cartReducer,
+  directory: directoryReducer,
 });
 
 export default persistReducer(persistConfig, rootReducer);
