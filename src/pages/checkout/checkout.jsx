4d0a8a45a40ec0fa59f1744ceddd6a99a79e64ed
Sixtus Innocent <sixtusinno@gmail.com>
2021-09-11 14:14:26 +0100

    Fixed git conflict

--- src/pages/checkout/checkout.jsx
+++ src/pages/checkout/checkout.jsx
@@ -2,7 +2,7 @@ import { connect } from "react-redux";
 import { createStructuredSelector } from "reselect";
 
 import { selectCartItems } from "../../redux/cart/cart-selectors";
-import CheckoutItem from "../../components/checkout-item/checkout-item";
+import ChekoutItem from "../../components/checkout-item/checkout-item";
 
 import "./checkout.scss";
 
@@ -26,7 +26,7 @@ const CheckoutPage = ({ cartItems }) => (
       </div>
     </div>
     {cartItems.map((cartItem) => (
-      <CheckoutItem key={cartItem.id} cartItem={cartItem} />
+      <ChekoutItem key={cartItem.id} cartItem={cartItem} />
     ))}
     <div className="total">TOTAL: $78</div>
   </div>
